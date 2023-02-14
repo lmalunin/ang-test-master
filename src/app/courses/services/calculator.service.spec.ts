7601339f9e695f2d47b3742546c399c4c86123f5
lmalunin <lmalunin@gmail.com>
2023-02-14 15:08:04 +0300

    5

---
+++ src/app/courses/services/calculator.service.spec.ts
@@ -0,0 +1,10 @@
+describe('CalculatorService', ()=>{
+
+    it('should be two numbers', ()=>{
+        pending();
+    });
+
+    it('should subtract two numbers', ()=>{
+        pending();
+    });
+})
