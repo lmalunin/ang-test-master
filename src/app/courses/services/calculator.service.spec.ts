8fa77add47471c019929880613405195a5dce529
lmalunin <lmalunin@gmail.com>
2023-02-15 15:20:01 +0300

    7

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -4,9 +4,11 @@ import { LoggerService } from "./logger.service";
 describe('CalculatorService', ()=>{
 
     it('should be two numbers', ()=>{
-        const calculatorService = new CalculatorService(new LoggerService());
+        const logger = jasmine.createSpyObj('LoggerService', ['log']);
+        const calculatorService = new CalculatorService(logger);
         const result = calculatorService.add(2,2);
         expect(result).toBe(4);
+        expect(logger.log).toHaveBeenCalledTimes(1);
     });
 
     it('should subtract two numbers', ()=>{
