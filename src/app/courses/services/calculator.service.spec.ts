e847bca1fb6931bd06a8e3cc8d5bd99dd5920832
lmalunin <lmalunin@gmail.com>
2023-02-16 14:36:19 +0300

    9

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -1,3 +1,4 @@
+import { TestBed } from "@angular/core/testing";
 import { CalculatorService } from "./calculator.service";
 import { LoggerService } from "./logger.service";
 
@@ -8,7 +9,15 @@ describe('CalculatorService', ()=>{
 
     beforeEach(() => {
         loggerSpy = jasmine.createSpyObj('LoggerService', ['log']);
-        calculatorService = new CalculatorService(loggerSpy);
+
+        TestBed.configureTestingModule({
+            providers: [
+                CalculatorService,
+                {provide: LoggerService, useValue: loggerSpy}
+            ]
+        });
+
+        calculatorService = TestBed.get(CalculatorService);
     })
 
     it('should be two numbers', ()=>{
