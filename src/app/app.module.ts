ad5bded1992515f149600ebb512d39cd297dc2c2
lmalunin <lmalunin@gmail.com>
2023-03-01 03:08:34 +0300

    3.1 3. Angular Component Testing In Depth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,13 @@
+import { HttpClientModule } from "@angular/common/http";
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from "@angular/material/button";
+import { MatToolbarModule } from "@angular/material/toolbar";
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoursesModule } from "./courses/courses.module";
 
 @NgModule({
   declarations: [
@@ -10,6 +15,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatToolbarModule,
+    MatButtonModule,
+    CoursesModule,
     AppRoutingModule
   ],
   providers: [],
