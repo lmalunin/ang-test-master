539c73f21688ba513641ae487e6a724a9b9d7705
lmalunin <lmalunin@gmail.com>
2023-03-09 15:55:38 +0300

    4.3

--- src/app/courses/home/async-examples.spec.ts
+++ src/app/courses/home/async-examples.spec.ts
@@ -1,3 +1,5 @@
+import { fakeAsync, tick } from "@angular/core/testing";
+
 fdescribe('Async Testing Examples', () => {
 
     it('Asynchronous test example with Jasmine done()', (done) => {
@@ -14,4 +16,19 @@ fdescribe('Async Testing Examples', () => {
         }, 1000);
     });
 
+    it('Asynchronous test example - setTimeout()', fakeAsync(() => {
+        let test = false;
+
+        setTimeout(() => {
+
+            console.log('running assertion setTimeout()');
+
+            test = true;
+
+
+        }, 0);
+
+        tick(0);
+        expect(test).toBeTruthy();
+    }));
 });
