0fe852c85e28b2d9ed95812b08b89cf7adef3b05
lmalunin <lmalunin@gmail.com>
2023-03-10 15:27:50 +0300

    5.3

--- cypress/integration/home.test.js
+++ cypress/integration/home.test.js
@@ -1,8 +1,12 @@
 describe('Home page', () => {
 
     it('should display a list of courses', () => {
+        cy.fixture('courses.json').as('coursesJSON');
+        cy.server();
+        cy.route('/api/courses', '@coursesJSON').as('courses');
         cy.visit('/');
-
         cy.contains('All Courses');
+        cy.wait('@courses');
+        cy.get("mat-card").should("have.length", 9);
     });
 });
