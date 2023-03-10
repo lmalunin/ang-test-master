80175dbe02c328a2bdf901f81c4494a148333e17
lmalunin <lmalunin@gmail.com>
2023-03-10 11:09:11 +0300

    2.4 no video

--- src/app/courses/services/courses.service.spec.ts
+++ src/app/courses/services/courses.service.spec.ts
@@ -1,6 +1,7 @@
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from "@angular/core/testing";
 import { COURSES } from "../../../../server/db-data";
+import { Course } from "../model/course";
 import { CoursesService } from "./courses.service";
 
 fdescribe('CoursesService', () => {
@@ -47,4 +48,30 @@ fdescribe('CoursesService', () => {
     afterEach(() => {
         httpTestingController.verify();
     })
+
+    it('should save the course data', () => {
+
+        const changes: Partial<Course> =
+            { titles: { description: 'Testing Course' } };
+
+        coursesService.saveCourse(12, changes)
+            .subscribe(course => {
+
+                expect(course.id).toBe(12);
+
+            });
+
+        const req = httpTestingController.expectOne('/api/courses/12');
+
+        expect(req.request.method).toEqual("PUT");
+
+        expect(req.request.body.titles.description)
+            .toEqual(changes?.titles?.description);
+
+        req.flush({
+            ...COURSES[12],
+            ...changes
+        })
+
+    });
 });
